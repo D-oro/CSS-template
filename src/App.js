16e50760ed1a4520f0666ecf5fb4ec9bb725efb6
Dorothea Hinrichs <dorothea.hinrichs@posteo.net>
2022-08-27 22:38:14 +0100

    beginning of a css template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import cats from './images/cats.png'
+import catlogo from './images/catlogo.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <header>
+        <text>Title of this beautiful webpage</text>
+        <div id="circle"><img src={catlogo} id="logo" alt="black cat logo"/></div>
       </header>
+
+      <main>
+        <img src={cats} id="photo" alt="two cats sitting at a corner"/>
+        <article id="maintext">This is the Article</article>
+        <aside id="list">This is the aside list</aside>
+      </main>
+
+      <footer>
+        this is the footer
+      </footer>
+
     </div>
   );
 }
 
 export default App;
+
